d52794ab82843c4edf45d8ef5495e91ee6515ec6
njc1583 <njc1583@gmail.com>
2019-09-20 23:08:00 -0500

    Update yaml file and index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const s3 = new AWS.S3({
   params: { Bucket: "mechmania2019" }
 });
 
-module.exports = authenticate(async (req, res) =>{
+module.exports = authenticate(async (req, res) => {
   const key = (req.url === "/") ? "" : req.url.slice(1);
   await mkdir(COMPILE_DIR);
   const data = s3
@@ -16,5 +16,4 @@ module.exports = authenticate(async (req, res) =>{
       .createReadStream();
 
   data.pipe(res);
-  res.download(COMPILE_DIR,data);
-}
+});
